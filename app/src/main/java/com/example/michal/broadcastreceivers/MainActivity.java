801987d28ec608c43a1eafbb64cef0d9ce2e2c6a
durden2 <gandor.michal@gmail.com>
2016-12-15 00:59:28 +0100

    Exercise

--- app/src/main/java/com/example/michal/broadcastreceivers/MainActivity.java
+++ app/src/main/java/com/example/michal/broadcastreceivers/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.michal.broadcastreceivers;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setAction("android.my.static");
-                sendBroadcast(intent);
+                // send intend as a broadcast
             }
         });
 
@@ -34,34 +29,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setAction("android.my.dynamic");
-                sendBroadcast(intent);
+                // send intend as a broadcast
             }
         });
     }
 
-    private IntentFilter filter =
-            new IntentFilter("android.my.dynamic");
-        //  Register a filter
+        //  Register a IntentFilter for 'android.my.dynamic'
 
-    private BroadcastReceiver broadcast = new BroadcastReceiver(){
-        @Override
-        public void onReceive(Context arg0, Intent arg1) {
-            Toast.makeText(arg0, "I received a dynamic message!", Toast.LENGTH_LONG).show();
-            Vibrator vibrator = (Vibrator) arg0.getSystemService(Context.VIBRATOR_SERVICE);
-            vibrator.vibrate(1000);
-        }
-    };
+
+        // Register a BroadcastReceiver doind the following
+        //    Toast.makeText(arg0, "I received a dynamic message!", Toast.LENGTH_LONG).show();
+        //    Vibrator vibrator = (Vibrator) arg0.getSystemService(Context.VIBRATOR_SERVICE);
+        //    vibrator.vibrate(1000);
+
 
     @Override
     public void onResume() {
         super.onResume();
-        registerReceiver(broadcast, filter);
         //Register our receiver in code
     }
 
     @Override
     public void onPause() {
-        unregisterReceiver(broadcast);
         // Unregister receiver
         super.onPause();
     }
